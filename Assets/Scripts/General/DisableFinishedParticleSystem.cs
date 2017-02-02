5dc87b27bb48da35a39dd6f85ea72cf51e065647
Jack Vine <jackv949@gmail.com>
2017-02-02 11:51:40 +1030

    Finished magic cast particle systems are now returned to the pool

---
+++ Assets/Scripts/General/DisableFinishedParticleSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableFinishedParticleSystem : MonoBehaviour
+{
+    //The particle system to get duration from
+    private ParticleSystem system;
+
+    void Awake()
+    {
+        //Get particle system (may be a component of a child gameobject)
+        system = GetComponentInChildren<ParticleSystem>();
+    }
+
+    void OnEnable()
+    {
+        //Disable the gameobject (for object pooling) after the particle system has finished
+        StartCoroutine("DisableAfterTime", system.main.duration);
+    }
+
+    IEnumerator DisableAfterTime(float time)
+    {
+        //Wait for time
+        yield return new WaitForSeconds(time);
+
+        //Disable (return to pool)
+        gameObject.SetActive(false);
+    }
+}
