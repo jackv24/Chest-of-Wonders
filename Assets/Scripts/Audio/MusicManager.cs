7922dd32e7baf7114f9abe653e77fd83eb302cb4
Jack Vine <jackv949@gmail.com>
2017-07-10 17:57:54 +0930

    Added BGM and crossfade

---
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+	public AudioSource primarySource;
+	public AudioSource secondarySource;
+
+	[Space()]
+	public List<AudioClip> music = new List<AudioClip>();
+
+	[Space()]
+	public float fadeDuration = 1.0f;
+
+	private void Start()
+	{
+		//Primary source should play one start
+		if (primarySource)
+		{
+			primarySource.volume = 1.0f;
+
+			if (music.Count > 0)
+			{
+				primarySource.clip = music[0];
+				primarySource.Play();
+			}
+		}
+
+		//Stop secondary source
+		if (secondarySource)
+		{
+			secondarySource.volume = 0.0f;
+			secondarySource.enabled = false;
+		}
+	}
+
+	private void Update()
+	{
+		//Temp code
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+			SwitchTo(0);
+		if (Input.GetKeyDown(KeyCode.Alpha2))
+			SwitchTo(1);
+	}
+
+	public void SwitchTo(int index)
+	{
+		if (music.Count > index)
+		{
+			//Only one fade coroutine should run at a time
+			StopCoroutine("FadeTo");
+			StartCoroutine(FadeTo(music[index], true));
+		}
+	}
+
+	IEnumerator FadeTo(AudioClip newClip, bool keepTime)
+	{
+		//No need to fade if the clip is the same
+		if (primarySource.clip == newClip)
+			yield return null;
+
+		//Switch current clip to secondary source at max volume
+		secondarySource.enabled = true;
+		secondarySource.volume = 1.0f;
+		secondarySource.clip = primarySource.clip;
+		secondarySource.time = primarySource.time;
+
+		secondarySource.Play();
+
+		//Put new clip in primary source at min volume
+		primarySource.volume = 0.0f;
+		primarySource.clip = newClip;
+		if(keepTime)
+			primarySource.time = secondarySource.time;
+
+		primarySource.Play();
+
+		//Crossfade audio sources
+		float elapsedTime = 0;
+		while(elapsedTime < fadeDuration)
+		{
+			yield return new WaitForEndOfFrame();
+			elapsedTime += Time.deltaTime;
+
+			primarySource.volume = Mathf.Lerp(0, 1.0f, elapsedTime / fadeDuration);
+			secondarySource.volume = Mathf.Lerp(1.0f, 0, elapsedTime / fadeDuration);
+		}
+
+		//Disable secondary source after crossfade
+		secondarySource.volume = 0;
+		secondarySource.enabled = false;
+
+		//Make sre primary source is at max volume
+		primarySource.volume = 1.0f;
+	}
+}
