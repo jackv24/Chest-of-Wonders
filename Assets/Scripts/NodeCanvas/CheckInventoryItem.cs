77f229f0f357d1e60d3d1be052b08a6e7fa9cc43
Jack Vine <jackv949@gmail.com>
2017-11-25 15:06:43 +1030

    Add CheckInventoryItem to custom condition tasks

---
+++ Assets/Scripts/NodeCanvas/CheckInventoryItem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NodeCanvas.Framework;
+
+namespace NodeCanvas.Tasks.Conditions
+{
+	public class CheckInventoryItem : ConditionTask
+	{
+		public InventoryItem itemToCheck;
+		public bool consume = false;
+
+		private PlayerInventory inventory;
+
+		protected override string OnInit()
+		{
+			inventory = GameObject.FindObjectOfType<PlayerInventory>();
+
+			return base.OnInit();
+		}
+
+		protected override bool OnCheck()
+		{
+			if (inventory && itemToCheck)
+			{
+				if (inventory.CheckItem(itemToCheck, consume))
+				{
+					return true;
+				}
+				else
+					return false;
+			}
+			else
+				return base.OnCheck();
+		}
+	}
+}
