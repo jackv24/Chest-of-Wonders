1e2d4dc336490d34f82eca20498f06247a1da088
Jack Vine <jackv949@gmail.com>
2017-11-25 18:39:32 +1030

    Added movecharacter action task

---
+++ Assets/Scripts/NodeCanvas/ActionTasks/MoveCharacter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NodeCanvas.Framework;
+
+namespace NodeCanvas.Tasks.Actions
+{
+	public class MoveCharacter : ActionTask
+	{
+		public BBParameter<float> distance = 1.0f;
+		public BBParameter<int> direction = 1;
+
+		private CharacterMove character;
+		private float initialPosX;
+		private float targetPosX;
+
+		protected override void OnExecute()
+		{
+			character = agent.GetComponent<CharacterMove>();
+
+			if (character)
+			{
+				initialPosX = character.transform.position.x;
+				targetPosX = character.transform.position.x + Mathf.Abs(distance.value) * Mathf.Sign(direction.value);
+			}
+			else
+				EndAction(false);
+		}
+
+		protected override void OnUpdate()
+		{
+			if(character)
+			{
+				character.Move(Mathf.Sign(direction.value));
+
+				if(Mathf.Abs(character.transform.position.x - initialPosX) >= Mathf.Abs(distance.value))
+				{
+					character.Move(0);
+					EndAction(true);
+				}
+			}
+		}
+	}
+}
