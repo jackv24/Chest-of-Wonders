83a15cb19e5bd4d2636e46980c8b61defc29ffe3
Jack Vine <jackv949@gmail.com>
2017-11-26 14:42:30 +1030

    Change Sootomander behaviour tree

---
+++ Assets/Scripts/NodeCanvas/ActionTasks/GetGameObjectDirection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NodeCanvas.Framework;
+
+namespace NodeCanvas.Tasks.Actions
+{
+	public class GetGameObjectDirection : ActionTask
+	{
+		public BBParameter<GameObject> gameObject;
+
+		public BBParameter<float> directionVar;
+
+		protected override void OnExecute()
+		{
+			if(gameObject.value)
+			{
+				float offsetX = gameObject.value.transform.position.x - agent.transform.position.x;
+
+				directionVar.value = Mathf.Sign(offsetX);
+			}
+
+			EndAction(true);
+		}
+	}
+}
