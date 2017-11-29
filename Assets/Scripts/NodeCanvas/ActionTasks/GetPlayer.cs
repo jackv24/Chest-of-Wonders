78a20dda5d6d51730c731982fe94f0b651b3e088
Jack Vine <jackv949@gmail.com>
2017-11-29 14:48:26 +1030

    Replaced FindPlayer behaviour tree with GetPlayer action task

---
+++ Assets/Scripts/NodeCanvas/ActionTasks/GetPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NodeCanvas.Framework;
+
+namespace NodeCanvas.Tasks.Actions
+{
+    public class GetPlayer : ActionTask
+    {
+        public BBParameter<GameObject> storeValue;
+
+        private GameObject player;
+
+        protected override string OnInit()
+        {
+            if(!player)
+            {
+                player = GameObject.FindWithTag("Player");
+            }
+
+            return base.OnInit();
+        }
+
+        protected override void OnExecute()
+        {
+            if(player && !storeValue.isNone)
+            {
+                storeValue.value = player;
+            }
+
+            EndAction(true);
+        }
+    }
+}
