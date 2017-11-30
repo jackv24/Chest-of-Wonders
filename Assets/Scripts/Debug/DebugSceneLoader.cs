6b4bad3b1b13d3c91c2c29a7182cf291c016f40e
Jack Vine <jackv949@gmail.com>
2017-11-30 21:13:52 +1030

    Added test parallax background

---
+++ Assets/Scripts/Debug/DebugSceneLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DebugSceneLoader : MonoBehaviour
+{
+	private InputField inputField;
+
+	private bool loading = false;
+
+	private void Start()
+	{
+		if (!Debug.isDebugBuild)
+			gameObject.SetActive(false);
+
+		inputField = GetComponent<InputField>();
+
+		if(inputField)
+		{
+			inputField.onEndEdit.AddListener((string input) =>
+			{
+				if (SceneUtility.GetBuildIndexByScenePath(input) > 1 && !loading)
+				{
+					loading = true;
+
+					SceneManager.LoadSceneAsync("Game", LoadSceneMode.Single);
+					SceneManager.LoadSceneAsync(input, LoadSceneMode.Additive);
+				}
+			});
+		}
+	}
+}
---
+++ Assets/Scripts/Effects/ParallaxBackground.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+	public float depthLimit = 10.0f;
+
+	public float divide = 32.0f;
+
+	private class Background
+	{
+		public Transform transform;
+		public MeshRenderer renderer;
+		public Vector2 initialPos;
+	}
+
+	private Background[] backgrounds;
+
+	private Transform mainCam;
+
+	private void Start()
+	{
+		MeshRenderer[] renderers = GetComponentsInChildren<MeshRenderer>();
+
+		backgrounds = new Background[renderers.Length];
+
+		for(int i = 0; i < backgrounds.Length; i++)
+		{
+			backgrounds[i] = new Background();
+
+			backgrounds[i].transform = renderers[i].transform;
+			backgrounds[i].renderer = renderers[i];
+			backgrounds[i].initialPos = renderers[i].transform.position;
+		}
+	}
+
+	private void Update()
+	{
+		if (mainCam)
+		{
+			Vector3 pos = mainCam.position;
+			pos.z = 0;
+
+			transform.position = pos;
+
+			for(int i = 0; i < backgrounds.Length; i++)
+			{
+				Vector2 offset = (Vector2)backgrounds[i].transform.position - backgrounds[i].initialPos;
+
+				float multiplier = Mathf.Lerp(1, 0, backgrounds[i].transform.position.z / depthLimit);
+
+				backgrounds[i].renderer.material.SetTextureOffset("_MainTex", (offset / divide) * multiplier);
+			}
+		}
+		else if(Camera.main)
+			mainCam = Camera.main.transform;
+	}
+}
