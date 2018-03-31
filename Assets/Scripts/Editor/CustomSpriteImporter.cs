2b3b5896ad79b4cd55bc500a7f14c0be291a7af5
Jack Vine <jackv949@gmail.com>
2018-03-31 16:49:43 +1030

    Added custom sprite import and batch pivot change menu option

---
+++ Assets/Scripts/Editor/CustomSpriteImporter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class CustomSpriteImporter : AssetPostprocessor
+{
+	void OnPreprocessTexture()
+	{
+		if (assetPath.StartsWith("Assets/Sprites"))
+		{
+			Debug.Log($"Preprocessing Texture: {assetPath}");
+
+			TextureImporter textureImporter = (TextureImporter)assetImporter;
+			textureImporter.spritePixelsPerUnit = 32;
+			textureImporter.filterMode = FilterMode.Point;
+		}
+	}
+}
