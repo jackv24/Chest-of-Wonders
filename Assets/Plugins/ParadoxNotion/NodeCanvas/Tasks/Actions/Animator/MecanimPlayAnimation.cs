539e8c775a1bd47333b84fd4ce120fe257fb0799
Jack Vine <jackv949@gmail.com>
2018-04-22 11:17:48 +0930

    NodeCanvas mecanim play anim default transition time changed to 0

--- Assets/Plugins/ParadoxNotion/NodeCanvas/Tasks/Actions/Animator/MecanimPlayAnimation.cs
+++ Assets/Plugins/ParadoxNotion/NodeCanvas/Tasks/Actions/Animator/MecanimPlayAnimation.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 
 namespace NodeCanvas.Tasks.Actions{
- 
+
     [Name("Play Animation")]
     [Category("Animator")]
     public class MecanimPlayAnimation : ActionTask<Animator> {
-     
+
         public BBParameter<int> layerIndex;
         [RequiredField]
         public BBParameter<string> stateName;
         [SliderField(0,1)]
-        public float transitTime = 0.25f;
+        public float transitTime = 0.0f;
         public bool waitUntilFinish;
-     
+
         private AnimatorStateInfo stateInfo;
         private bool played;
-     
+
         protected override string info{
             get {return "Anim '" + stateName.ToString() + "'";}
         }
-     
+
         protected override void OnExecute(){
             if (string.IsNullOrEmpty(stateName.value) || !agent.HasState(layerIndex.value, Animator.StringToHash(stateName.value))){
                 EndAction(false);
@@ -32,25 +32,25 @@ namespace NodeCanvas.Tasks.Actions{
             var current = agent.GetCurrentAnimatorStateInfo(layerIndex.value);
             agent.CrossFade(stateName.value, transitTime/current.length, layerIndex.value);
         }
-     
+
         protected override void OnUpdate(){
-         
+
             stateInfo = agent.GetCurrentAnimatorStateInfo(layerIndex.value);
-         
+
             if (waitUntilFinish){
-             
+
                 if (stateInfo.IsName(stateName.value)){
-            
+
 					played = true;
                     if (elapsedTime >= (stateInfo.length / agent.speed)){
-                        EndAction();              
+                        EndAction();
                     }
 
                 } else if (played) {
 
                     EndAction();
                 }
-             
+
             } else {
 
                 if (elapsedTime >= transitTime){
