1309b1a16d7013e6a5b55d8ad37dda3313723352
Jack Vine <jackv949@gmail.com>
2018-04-25 22:33:25 +0930

    Added Give Inventory Item task action

--- Assets/Scripts/NodeCanvas/ConditionTasks/CheckInventoryItem.cs
+++ Assets/Scripts/NodeCanvas/ConditionTasks/CheckInventoryItem.cs
@@ -2,19 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using NodeCanvas.Framework;
+using ParadoxNotion.Design;
 
 namespace NodeCanvas.Tasks.Conditions
 {
+	[Category("Player/Inventory")]
 	public class CheckInventoryItem : ConditionTask
 	{
-		public InventoryItem itemToCheck;
-		public bool consume = false;
+		public BBParameter<InventoryItem> itemToCheck;
+		public BBParameter<bool> consume = false;
 
 		protected override bool OnCheck()
 		{
-			if (PlayerInventory.Instance && itemToCheck)
+			if (PlayerInventory.Instance && !itemToCheck.isNone)
 			{
-				if (PlayerInventory.Instance.CheckItem(itemToCheck, consume))
+				if (PlayerInventory.Instance.CheckItem(itemToCheck.value, consume.value))
 				{
 					return true;
 				}
