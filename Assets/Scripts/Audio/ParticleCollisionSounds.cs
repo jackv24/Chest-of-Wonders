1b4451dcac2e10c62a79541692a5d056fea79ddf
Jack Vine <jackv949@gmail.com>
2018-06-10 16:39:05 +0930

    Added particle collision sounds

---
+++ Assets/Scripts/Audio/ParticleCollisionSounds.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Plays a sound when a particle collision occurs. Must be attached to the same GameObject as the ParticleSystem.
+/// </summary>
+public class ParticleCollisionSounds : MonoBehaviour
+{
+	public SoundType soundType;
+	public SoundEventRandom collisionSounds;
+
+	private List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>();
+
+	private ParticleSystem system;
+
+	private void Awake()
+	{
+		system = GetComponent<ParticleSystem>();
+	}
+
+	private void OnParticleCollision(GameObject other)
+	{
+		int eventCount = system.GetCollisionEvents(other, collisionEvents);
+
+		for(int i = 0; i < eventCount; i++)
+		{
+			collisionSounds.Play(collisionEvents[i].intersection, soundType);
+		}
+	}
+}
