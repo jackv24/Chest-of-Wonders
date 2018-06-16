30c18f0d642d23cbcf2750ee545b8bb98088c7dd
Jack Vine <jackv949@gmail.com>
2018-06-16 12:42:40 +0930

    Refactored doorways system

---
+++ Assets/Scripts/Level/SpawnMarker.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMarker : MonoBehaviour
+{
+	public virtual Vector2 SpawnPosition => (Vector2)transform.position + spawnOffset;
+
+	[SerializeField]
+	private Vector2 spawnOffset;
+}
