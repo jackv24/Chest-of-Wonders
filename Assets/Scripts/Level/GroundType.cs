30522ddf839e3bb4676a9322ba19f9b1400ba025
Jack Vine <jackv949@gmail.com>
2018-06-21 09:32:50 +0930

    New branch emptied for gh-pages and added index.html

--- Assets/Scripts/Level/GroundType.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New GroundType", menuName = "Data/Ground Type")]
-public class GroundType : ScriptableObject
-{
-	//This class is currently used as an alternative to hard-coded enums
-}
--- Assets/Scripts/TagSelectorAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TagSelectorAttribute : PropertyAttribute
-{
-    public bool useDefaultTagFieldDrawer = false;
-}
