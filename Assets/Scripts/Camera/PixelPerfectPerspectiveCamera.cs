992fa25134936145f97cbb13ed2d56eb4d06e340
Jack Vine <jackv949@gmail.com>
2018-07-21 13:34:31 +0930

    Started testing pixel perfect perspective camera

---
+++ Assets/Scripts/Camera/PixelPerfectPerspectiveCamera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PixelPerfectPerspectiveCamera : MonoBehaviour
+{
+	public const float pixelsPerUnit = 32.0f;
+
+	private Camera cam;
+
+	private void Start()
+	{
+		cam = GetComponent<Camera>();
+
+		UpdatePosition();
+	}
+
+	private void Update()
+	{
+		if(!Application.isPlaying)
+		{
+			UpdatePosition();
+		}
+	}
+
+	private void UpdatePosition()
+	{
+		var targetFrustWidth = 360.0f / pixelsPerUnit;
+		var frustumInnerAngles = (180f - cam.fieldOfView) / 2f * Mathf.PI / 180f;
+		var newCamDist = Mathf.Tan(frustumInnerAngles) * (targetFrustWidth / 2);
+		transform.SetLocalPositionZ(-newCamDist);
+	}
+}
---
+++ Assets/Scripts/Camera/PixelPerfectPerspectiveObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PixelPerfectPerspectiveObject : MonoBehaviour
+{
+	public const float pixelsPerUnit = PixelPerfectPerspectiveCamera.pixelsPerUnit;
+
+	public Vector2 multiplier = Vector2.one;
+
+	public bool updateEveryFrame = false;
+
+	private void Start()
+	{
+		UpdateSize();
+	}
+
+	private void Update()
+	{
+		if(!Application.isPlaying || updateEveryFrame)
+		{
+			UpdateSize();
+		}
+	}
+
+	private void UpdateSize()
+	{
+		var a = Camera.main.WorldToScreenPoint(transform.position);
+		var b = new Vector3(a.x, a.y + pixelsPerUnit, a.z);
+
+		var aa = Camera.main.ScreenToWorldPoint(a);
+		var bb = Camera.main.ScreenToWorldPoint(b);
+
+		transform.localScale = multiplier * (aa - bb).magnitude;
+	}
+}
