f6a46915f2382967c856c99e02cd916341afe1fd
Jack Vine <jackv949@gmail.com>
2018-07-21 14:13:58 +0930

    Changed perspective object math to be more precise

--- Assets/Scripts/Camera/PixelPerfectPerspectiveObject.cs
+++ Assets/Scripts/Camera/PixelPerfectPerspectiveObject.cs
@@ -26,12 +26,28 @@ public class PixelPerfectPerspectiveObject : MonoBehaviour
 
 	private void UpdateSize()
 	{
-		var a = Camera.main.WorldToScreenPoint(transform.position);
-		var b = new Vector3(a.x, a.y + pixelsPerUnit, a.z);
+		//var a = Camera.main.WorldToScreenPoint(transform.position);
+		//var b = new Vector3(a.x, a.y + pixelsPerUnit, a.z);
 
-		var aa = Camera.main.ScreenToWorldPoint(a);
-		var bb = Camera.main.ScreenToWorldPoint(b);
+		//var aa = Camera.main.ScreenToWorldPoint(a);
+		//var bb = Camera.main.ScreenToWorldPoint(b);
 
-		transform.localScale = multiplier * (aa - bb).magnitude;
+		//transform.localScale = multiplier * (aa - bb).magnitude;
+
+		Camera cam = Camera.main;
+
+		float cameraDistance = -cam.transform.position.z;
+		float objectDistance = transform.position.z - cam.transform.position.z;
+
+		float frustumHeightOrigin = 2.0f * cameraDistance * Mathf.Tan(cam.fieldOfView * 0.5f * Mathf.Deg2Rad);
+		float frustumHeightObject = 2.0f * objectDistance * Mathf.Tan(cam.fieldOfView * 0.5f * Mathf.Deg2Rad);
+
+		float difference = frustumHeightObject - frustumHeightOrigin;
+		float size = difference * (32f/360f) + 1;
+
+		Vector3 scale = multiplier * size;
+		scale.z = transform.localScale.z;
+
+		transform.localScale = scale;
 	}
 }
