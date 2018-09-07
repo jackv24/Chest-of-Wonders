4b6a974b11bb01b99cf6c0ce7bc8a8e4a00b6325
Jack Vine <jackv949@gmail.com>
2018-09-07 22:08:57 +0930

    Fixed missing behaviours (after removing nested prefabs)

--- Assets/Scripts/Editor/FindMissingScripts.cs
+++ Assets/Scripts/Editor/FindMissingScripts.cs
@@ -4,8 +4,8 @@ using UnityEngine.SceneManagement;
 
 public class FindMissingScripts
 {
-	[MenuItem("Overgrowth Tools/Find Missing Scripts in Scene")]
-	public static void FindMissingComponents()
+	[MenuItem("Overgrowth Tools/Remove Missing Scripts in Scenes")]
+	public static void FindMissingComponentsInScenes()
 	{
 		bool wasFound = false;
 
@@ -23,14 +23,43 @@ public class FindMissingScripts
 			Debug.Log("No missing components were found in any open scenes!");
 	}
 
+	[MenuItem("Overgrowth Tools/Remove Missing Scripts in Project")]
+	public static void FindMissingComponentsInProject()
+	{
+		if (!EditorUtility.DisplayDialog("Are you sure?", "This may take a while, and will touch every GameObject in the project!", "YES", "NO"))
+			return;
+
+		bool wasFound = false;
+
+		string[] guids = AssetDatabase.FindAssets("t:GameObject");
+		foreach(var guid in guids)
+		{
+			string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+			GameObject obj = AssetDatabase.LoadAssetAtPath<GameObject>(assetPath);
+			if (obj)
+				FindMissingComponentsInGameObject(obj, true, ref wasFound);
+		}
+
+		if (!wasFound)
+			Debug.Log("No missing components were found on GameObjects in project!");
+	}
+
 	private static void FindMissingComponentsInGameObject(GameObject obj, bool recursive, ref bool wasFound)
 	{
+		int removedCount = 0;
 		Component[] components = obj.GetComponents<Component>();
 		for(int i = 0; i < components.Length; i++)
 		{
 			if(components[i] == null)
 			{
 				wasFound = true;
+
+				// Remove null component
+				var serializedObject = new SerializedObject(obj);
+				var property = serializedObject.FindProperty("m_Component");
+				property.DeleteArrayElementAtIndex(i - removedCount);
+				removedCount++;
+				serializedObject.ApplyModifiedProperties();
 
 				// Get GameObject hierarchy path by looping up through parents
 				string path = obj.name;
