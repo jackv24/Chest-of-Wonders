9f3be291bddf5b52fa8de94a16880b395e2f5022
Jack Vine <jackv949@gmail.com>
2018-09-23 11:52:23 +0930

    Fixed options menu display

---
+++ Assets/Scripts/UI/VerticalNavigationGroup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using SubjectNerd.Utilities;
+
+public class VerticalNavigationGroup : MonoBehaviour
+{
+	[Reorderable]
+	public List<Selectable> selectables;
+
+	private void OnEnable()
+	{
+		List<Selectable> selectables = this.selectables
+			.Where(selectable => selectable.gameObject.activeInHierarchy)
+			.ToList();
+
+		for(int i = 0; i < selectables.Count; i++)
+		{
+			Selectable previous = i > 0 ? selectables[i - 1] : null;
+			Selectable next = i < selectables.Count - 1 ? selectables[i + 1] : null;
+
+			Navigation nav = selectables[i].navigation;
+			nav.selectOnUp = previous;
+			nav.selectOnDown = next;
+			selectables[i].navigation = nav;
+		}
+	}
+}
