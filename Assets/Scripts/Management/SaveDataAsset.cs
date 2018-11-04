d371e807a4513a9c90b74d434b3cd5e9264f4cc0
Jack Vine <jackv949@gmail.com>
2018-11-04 22:46:51 +1030

    Fixed savedata persisting between plays in same editor session

--- Assets/Scripts/Management/SaveDataAsset.cs
+++ Assets/Scripts/Management/SaveDataAsset.cs
@@ -1,9 +1,26 @@
 ﻿using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 [CreateAssetMenu(menuName = "Data/Save Data Asset")]
 public class SaveDataAsset : ScriptableObject
 {
     [SerializeField]
     private SaveData data;
-    public SaveData Data { get { return data; } }
+
+    public SaveData Data
+    {
+        get
+        {
+            using (var ms = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(ms, data);
+                ms.Position = 0;
+
+                return (SaveData)formatter.Deserialize(ms);
+            }
+        }
+    }
 }
