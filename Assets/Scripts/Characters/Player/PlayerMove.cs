91028310917096811e19712a4e00d7e09acc0f02
Jack Vine <jackv949@gmail.com>
2018-11-07 15:26:01 +1030

    Add min start dash mana amount
    Prevents player from rapidly switching to and from dashing when mana reaches 0 and then recharges

--- Assets/Scripts/Characters/Player/PlayerMove.cs
+++ Assets/Scripts/Characters/Player/PlayerMove.cs
@@ -13,12 +13,16 @@ public class PlayerMove : CharacterMove
     private float dashManaDrain;
     private PlayerStats playerStats;
 
-    public bool IsDashing { get; private set; }
+    [SerializeField]
+    private int minStartDashMana = 10;
+    private bool hasManaBlockedDash;
 
     protected override float MoveSpeedMultiplier
     {
         get { return IsDashing ? dashSpeedMultiplier : 1.0f; }
     }
+
+    public bool IsDashing { get; private set; }
 
     protected override void Awake()
     {
@@ -29,20 +33,28 @@ public class PlayerMove : CharacterMove
 
     public bool Move(float direction, bool shouldDash)
     {
+        bool didMove = Move(direction);
+        bool doMinManaTest = hasManaBlockedDash || (!IsDashing && shouldDash);
+
         // Only do dash and mana drain if we can move
-        if (Move(direction))
+        if (didMove)
         {
             IsDashing = shouldDash;
 
             if (IsDashing)
             {
-                if (direction == 0 || playerStats.CurrentMana <= 0)
+                // Can only START dashing if mana is above a certain amount (but if already dashing can continue to drain until run out)
+                bool blockDash = doMinManaTest && playerStats.CurrentMana < minStartDashMana;
+
+                if (direction == 0 || playerStats.CurrentMana <= 0 || blockDash)
                 {
                     dashManaDrain = 0;
                     IsDashing = false;
+                    hasManaBlockedDash = true;
                 }
                 else
                 {
+                    hasManaBlockedDash = false;
                     dashManaDrain += dashManaDrainSpeed * Time.deltaTime;
                     if (dashManaDrain > 1.0f)
                     {
@@ -51,10 +63,8 @@ public class PlayerMove : CharacterMove
                     }
                 }
             }
-
-            return true;
         }
 
-        return false;
+        return didMove;
     }
 }
